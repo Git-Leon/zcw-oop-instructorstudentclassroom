cdd0b3741429c538fe8996170d0daa8dac772d65
Leon <leon@Leons-MacBook-Pro.local>
2017-09-25 20:51:59 -0400

    I think I'm done...

--- src/main/java/io/zipcoder/interfaces/Instructors.java
+++ src/main/java/io/zipcoder/interfaces/Instructors.java
@@ -3,27 +3,18 @@ package io.zipcoder.interfaces;
 /**
  * Created by Chris on 9/23/2017.
  */
-public final class Instructors extends People {
-    private static final Instructors INSTANCE = new Instructors();
-    private Instructors() {
-        for (Educator educator : Educator.values()) {
-            add(new Instructor(educator));
-        }
+public final class Educators extends People<Instructor> {
+    private static final Educators INSTANCE = new Educators();
+
+    private Educators() {
     }
 
-    public static Instructors getInstance() {
+    public static Educators getInstance() {
         return INSTANCE;
     }
 
-
-    // TODO - Replace with generic inheritance to prevent odd casting issues of subclasses
-    public Instructor[] getInstructors() {
-        int numberOfInstructors = getCount();
-        Instructor[] instructors = new Instructor[numberOfInstructors];
-        for(int i=0; i<people.size(); i++) {
-            Person currentPerson = people.get(i);
-            instructors[i] = (Instructor) currentPerson;
-        }
-        return instructors;
+    @Override
+    public Instructor[] getArray() {
+        return people.stream().toArray(Instructor[]::new);
     }
 }
---
+++ src/test/java/io/zipcoder/interfaces/TestZipCodeWilmington.java
@@ -0,0 +1,7 @@
+package io.zipcoder.interfaces;
+
+/**
+ * Created by leon on 9/25/17.
+ */
+public class TestZipCodeWilmington {
+}
